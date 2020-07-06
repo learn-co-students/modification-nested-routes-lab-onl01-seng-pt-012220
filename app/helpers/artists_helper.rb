2937533ef52f2ae06f2ef28d4f21992889da04ff
vanessuniq <vanessuniq@gmail.com>
2020-07-05 21:52:27 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def display_name(song, path)
+    song.artist_name if path == 'nested'
+  end
 end
